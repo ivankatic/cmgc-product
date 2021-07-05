f9a3bb9d751c685de4459bb102826f56a9206549
Ivan Katić <info@rockbell.studio>
2021-07-06 00:28:09 +0200

    Add homepage and components boilerplate

---
+++ components/ProductDetails.tsx
@@ -0,0 +1,18 @@
+const ProductDetails = () => {
+    return(
+        <div>
+            <h1>Lorem ipsum</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi optio quis ipsa in suscipit blanditiis esse reiciendis deleniti facilis. Vitae assumenda laborum sequi maxime reprehenderit commodi quae aut incidunt veniam.</p>
+            <button>Buy now</button>
+            <ul>
+                <li>feature 1</li>
+                <li>feature 1</li>
+                <li>feature 1</li>
+                <li>feature 1</li>
+                <li>feature 1</li>
+            </ul>
+        </div>
+    );
+}
+
+export default ProductDetails;
