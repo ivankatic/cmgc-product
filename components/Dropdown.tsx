26993058d7888552bd2e6bfc4a6be4d82b3295d6
Ivan Katić <info@rockbell.studio>
2021-07-06 18:47:49 +0200

    Fix react-select type issues.

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -3,7 +3,7 @@ import Select from 'react-select';
 import { ValueType } from 'react-select';
 import classes from './Dropdown.module.css';
 
-type OptionType = { label: string; value: number };
+type OptionType = { label: string; value: string };
 
 interface Dropdown {
 	sizes: string[];
@@ -11,11 +11,13 @@ interface Dropdown {
 }
 
 const Dropdown: React.FC<Dropdown> = (props) => {
-	const changeHandler = (selected: OptionType) => {
-		props.onChange(selected.value.toString());
+	const changeHandler = (selected: ValueType<OptionType, false>) => {
+		if (selected) {
+			props.onChange(selected.value);
+		}
 	};
 
-	let options = [];
+	let options: OptionType[] = [];
 
 	for (let i = 0; i < props.sizes.length; i++) {
 		options.push({
@@ -30,8 +32,8 @@ const Dropdown: React.FC<Dropdown> = (props) => {
 			<Select
 				instanceId='sizes'
 				defaultValue={options[0]}
-				onChange={(selectedOption: ValueType<OptionType>) => {
-					changeHandler(selectedOption);
+				onChange={(selectedOption) => {
+					changeHandler(selectedOption as OptionType);
 				}}
 				options={options}
 				className={classes.dropdown}
